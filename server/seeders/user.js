fc64c1d79845d71b671f8f1905a3f0979f01bc4c
Eri Irawan <email": null>
2017-03-12 17:26:07 +0700

    add seeder for user

---
+++ server/seeders/user.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var data = [{
+  username: 'eri',
+  skills: [{
+    name: 'Coding',
+    score: 'Beginner'
+  },
+    {
+      name: 'Game',
+      score: 'Advanced'
+    }
+  ]
+},
+  {
+    username: 'rubi',
+    skills: [{
+      name: 'Coding',
+      score: 'Professional'
+    },
+      {
+        name: 'Futsal',
+        score: 'Professional'
+      },
+      {
+        name: 'Teaching',
+        score: 'Professional'
+      }
+    ]
+  },
+  {
+    username: 'windi',
+    skills: [{
+      name: 'Coding',
+      score: 'Professional'
+    },
+      {
+        name: 'Reading',
+        score: 'Amateur'
+      }
+    ]
+  }
+]
+
+module.exports = data
