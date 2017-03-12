39835d7e64e800a96897a34d29f64e41f2b24aa5
Eri Irawan <email": null>
2017-03-12 21:01:50 +0700

    add endpoint for skill

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,12 @@ const controller = require('../controllers/userController')
 
 router.post('/seed', controller.seeders)
 router.get('/users', controller.getUsers)
-router.get('/user/:id', controller.getOneUser)
+router.get('/user/:username', controller.getOneUser)
 router.post('/user', controller.createUser)
-router.put('/user/:id', controller.updateUser)
-router.delete('/user/:id', controller.deleteUser)
+router.put('/user/:username', controller.updateUser)
+router.delete('/user/:username', controller.deleteUser)
+
+router.get('/:username/skills', controller.getSkills)
+router.put('/:username/skill', controller.addSkill)
 
 module.exports = router
