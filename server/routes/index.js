143d07cebcc288f782a073536ff988884f48b3dd
Eri Irawan <email": null>
2017-03-12 17:25:42 +0700

    add router for user

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/userController')
 
+router.post('/seed', controller.seeders)
 router.get('/users', controller.getUsers)
-// router.post('/user', controller.addTodo)
-// router.put('/user/:id', controller.editTodo)
-// router.delete('/user/:id', controller.removeTodo)
+router.get('/user/:id', controller.getOneUser)
+router.post('/user', controller.createUser)
+router.put('/user/:id', controller.updateUser)
+router.delete('/user/:id', controller.deleteUser)
 
 module.exports = router
